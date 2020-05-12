d15c21a35b72b676b89df9d1f96e53a334d56746
Sharon Lobo <slobo@unal.edu.co>
2020-05-11 21:07:11 -0500

    Añadido menú del programa.

--- Calculadora_de_matrices.py
+++ Calculadora_de_matrices.py
@@ -14,3 +14,16 @@ if n > 0:
                 else:
                     matriz2[i, j] = float(input(f"Introduzca valor de la casilla {i + 1}, {j + 1} para la matriz {h + 1}: "))
                     print(matriz2)
+
+    print("Calculadora de matrices \n", "Menú: \n")
+
+    print("1) Sumar matriz 1 y matriz 2")
+    print("2) Restar matriz 1 y matriz 2")
+    print("3) Multiplicar matriz 1 y matriz 2")
+    print("4) Transpuesta de la matriz 1")
+    print("5) Transpuesta de la matriz 2")
+    print("6) Número mayor y menor de la matriz 1")
+    print("7) Número mayor y menor de la matriz 2")
+    print("8) Salir del programa")
+
+    m = int(input("Introduzca la opción que desea: "))
