c05e283f990c98c9e8479cde4bd28bbd8efc3c58
Luis <lefelite@gmail.com>
2020-05-11 19:17:31 -0500

    Se añadió el código para llenar las matrices.

--- Calculadora_de_matrices.py
+++ Calculadora_de_matrices.py
@@ -1,3 +1,16 @@
 import numpy as np
 
 n = int(input("Introduzca valor de n para las matrices: "))
+
+if n > 0:
+    matriz1 = np.zeros((n, n))
+    matriz2 = np.zeros((n, n))
+    for h in range(2):        
+        for i in range(n):
+            for j in range(n):
+                if h == 0:
+                    matriz1[i, j] = float(input(f"Introduzca valor de la casilla {i + 1}, {j + 1} para la matriz {h + 1}: "))
+                    print(matriz1)
+                else:
+                    matriz2[i, j] = float(input(f"Introduzca valor de la casilla {i + 1}, {j + 1} para la matriz {h + 1}: "))
+                    print(matriz2)
