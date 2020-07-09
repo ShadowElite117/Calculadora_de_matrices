68c62539929a4069957397045240d2f15be2887a
Luis Ferreira <lefelite@gmail.com>
2020-07-09 14:25:29 -0500

    Ahora ya se pueden recibir más de 2 matrices al tiempo.

--- Calculadora_de_matrices.py
+++ Calculadora_de_matrices.py
@@ -217,15 +217,13 @@ def menu():
     while True:
         print("Calculadora de matrices \n", "Menú: \n")
 
-        print("1) Sumar matriz 1 y matriz 2")
-        print("2) Restar matriz 1 y matriz 2")
-        print("3) Multiplicar matriz 1 y matriz 2")
-        print("4) Transpuesta de la matriz 1")
-        print("5) Transpuesta de la matriz 2")
-        print("6) Número menor y mayor de la matriz 1")
-        print("7) Número menor y mayor de la matriz 2")
-        print("8) Graficar matrices")
-        print("9) Salir del programa")
+        print("1) Introducir matrices")
+        print("2) Sumar matrices")
+        print("3) Restar matrices")
+        print("4) Multiplicar matrices")
+        print("5) Transpuesta de la matriz")
+        print("6) Número menor y mayor de la matriz")
+        print("7) Salir del programa")
 
         try:
             opcion = int(input("Introduzca la opción que desea: "))
@@ -235,13 +233,13 @@ def menu():
         else:
             return opcion
 
-def dimensiones_matriz_1():
-    dim_matriz_1 = [0, 0]
+def dimensiones_matriz(num_matriz):
+    dim_matriz = [0, 0]
 
     while True:
         try:
-            dim_matriz_1[0] = int(input("Introduzca valor de filas (i) para la matriz 1: "))
-            if dim_matriz_1[0] <= 0:
+            dim_matriz[0] = int(input(f"Introduzca valor de filas (i) para la matriz {num_matriz}: "))
+            if dim_matriz[0] <= 0:
                 raise DimensionNoEsMayorACero
         except ValueError:
             borrar_pantalla()
@@ -252,8 +250,8 @@ def dimensiones_matriz_1():
         else:
             while True:
                 try:
-                    dim_matriz_1[1] = int(input("Introduzca valor de filas (j) para la matriz 1: "))
-                    if dim_matriz_1[1] <= 0:
+                    dim_matriz[1] = int(input(f"Introduzca valor de filas (j) para la matriz {num_matriz}: "))
+                    if dim_matriz[1] <= 0:
                         raise DimensionNoEsMayorACero
                 except ValueError:
                     borrar_pantalla()
@@ -266,46 +264,12 @@ def dimensiones_matriz_1():
 
             break
 
-    return dim_matriz_1
+    return dim_matriz
 
-def dimensiones_matriz_2():
-    dim_matriz_2 = [0, 0]
+def crear_matriz(dim_matriz):
+    matriz = np.zeros((dim_matriz[0], dim_matriz[1]))
 
-    while True:
-        try:
-            dim_matriz_2[0] = int(input("Introduzca valor de filas (i) para la matriz 2: "))
-            if dim_matriz_2[0] <= 0:
-                raise DimensionNoEsMayorACero
-        except ValueError:
-            borrar_pantalla()
-            print("Por favor introduzca un número entero positivo mayor a cero.")
-        except DimensionNoEsMayorACero:
-            borrar_pantalla()
-            print("Por favor introduzca un número entero positivo mayor a cero.")
-        else:
-            while True:
-                try:
-                    dim_matriz_2[1] = int(input("Introduzca valor de filas (j) para la matriz 2: "))
-                    if dim_matriz_2[1] <= 0:
-                        raise DimensionNoEsMayorACero
-                except ValueError:
-                    borrar_pantalla()
-                    print("Por favor introduzca un número entero positivo mayor a cero.")
-                except DimensionNoEsMayorACero:
-                    borrar_pantalla()
-                    print("Por favor introduzca un número entero positivo mayor a cero.")
-                else:
-                    break
-
-            break
-
-    return dim_matriz_2
-
-def crear_matrices(dim_matriz_1, dim_matriz_2):
-    matriz_1 = np.zeros((dim_matriz_1[0], dim_matriz_1[1]))
-    matriz_2 = np.zeros((dim_matriz_2[0], dim_matriz_2[1]))
-
-    return matriz_1, matriz_2
+    return matriz
 
 def llenar_matriz(dim_matriz, matriz):     
     for i in range(dim_matriz[0]):
@@ -320,6 +284,20 @@ def llenar_matriz(dim_matriz, matriz):
                     break
 
     return matriz
+
+def numero_matrices(matrices):
+    while True:
+        try:
+            borrar_pantalla()
+            print("Hay disponibles %d matrices." %(len(matrices)))
+            num_matriz_1 = int(input("Seleccione matriz 1: "))
+            num_matriz_2 = int(input("Seleccione matriz 2: "))
+        except ValueError:
+            continue
+        else:
+            break
+
+    return num_matriz_1, num_matriz_2
 
 # Se suman las matrices 1 y 2, y se retorna la matriz respuesta.
 def suma_matrices(matriz_1, dim_matriz_1, matriz_2):
@@ -372,85 +350,121 @@ def numero_menor_mayor(matriz, dim_matriz, menor, mayor):
 
     return menor, mayor
 
-def opciones_menu(opcion, matriz_1, dim_matriz_1, matriz_2, dim_matriz_2):
+def opciones_menu(opcion, matrices, matriz_respuesta):
+    borrar_pantalla()
+
     if opcion == 1:
-        if dim_matriz_1 == dim_matriz_2:
-            print(suma_matrices(matriz_1, dim_matriz_1, matriz_2), "\n")
+        continuar = True
+        num_matriz = 0
+
+        while continuar == True:
+            matrices[num_matriz].append(dimensiones_matriz(num_matriz + 1))
+            matrices[num_matriz].append(crear_matriz(matrices[num_matriz][0]))
+
+            matrices[num_matriz][1] = llenar_matriz(matrices[num_matriz][0], matrices[num_matriz][1])
+
+            while True:
+                op = str(input("¿Desea agregar otra matriz? (Si/No): "))
+
+                if op == "SI" or op == "Si" or op == "si":
+                    num_matriz += 1
+                    matrices.append([])
+                    break
+                elif op == "NO" or op == "No" or op == "no":
+                    continuar = False
+                    break
+                else:
+                    continue
+
+        return matrices, matriz_respuesta, True
+
+    if opcion == 2:
+        matriz_1, matriz_2 = numero_matrices(matrices)
+
+        if matrices[matriz_1 - 1][0] == matrices[matriz_2 - 1][0]:
+            matriz_respuesta = suma_matrices(matrices[matriz_1 - 1][1], matrices[matriz_1 - 1][0], matrices[matriz_2 - 1][1])
+            print(matriz_respuesta, "\n")
+            input("Presione Enter para continuar...")
         else:
             print("Las matrices 1 y 2 no se pueden sumar porque sus dimensiones son diferentes.")
 
-    if opcion == 2:
-        print("1) Calcular matriz 1 - matriz 2")
-        print("2) Calcular matriz 2 - matriz 1")
-        op = int(input("Ingrese la operacion que desee realizar: "))
-
-        if op == 1:
-            if dim_matriz_1 == dim_matriz_2:
-                print(resta_matrices(matriz_1, dim_matriz_1, matriz_2), "\n")
-            else:
-                print("Las matrices 1 y 2 no se pueden restar porque sus dimensiones son diferentes.")
-        else:
-            if dim_matriz_1 == dim_matriz_2:
-                print(resta_matrices(matriz_2, dim_matriz_1, matriz_1), "\n")
-            else:
-                print("Las matrices 2 y 1 no se pueden restar porque sus dimensiones son diferentes.")
+        return matrices, matriz_respuesta, True
 
     if opcion == 3:
-        print("1) Calcular matriz 1 x matriz 2")
-        print("2) Calcular matriz 2 x matriz 1")
-        op = int(input("Introduzca la opción que desea: "))
+        matriz_1, matriz_2 = numero_matrices(matrices)
 
-        if op == 1:
-            if dim_matriz_1[1] == dim_matriz_2[0]:
-                print(multiplicacion_matrices(matriz_1, dim_matriz_1, matriz_2, dim_matriz_2), "\n")
-            else:
-                print("Las matrices 1 y 2 no se pueden multiplicar porque el número de columnas")
-                print("de la matriz 1 es diferente al número de filas de la matriz 2.")
+        if matrices[matriz_1 - 1][0] == matrices[matriz_2 - 1][0]:
+            matriz_respuesta = resta_matrices(matrices[matriz_1 - 1][1], matrices[matriz_1 - 1][0], matrices[matriz_2 - 1][1])
+            print(matriz_respuesta, "\n")
+            input("Presione Enter para continuar...")
         else:
-            if dim_matriz_2[1] == dim_matriz_1[0]:
-                print(multiplicacion_matrices(matriz_2, dim_matriz_2, matriz_1, dim_matriz_1), "\n")
-            else:
-                print("Las matrices 2 y 1 no se pueden multiplicar porque el número de columnas")
-                print("de la matriz 2 es diferente al número de filas de la matriz 1.")
+            print("Las matrices 1 y 2 no se pueden restar porque sus dimensiones son diferentes.")
+
+        return matrices, matriz_respuesta, True
 
     if opcion == 4:
-        print(transpuesta(matriz_1, dim_matriz_1), "\n")
+        matriz_1, matriz_2 = numero_matrices(matrices)
+
+        if matrices[matriz_1 - 1][0][1] == matrices[matriz_2 - 1][0][0]:
+            matriz_respuesta = multiplicacion_matrices(matrices[matriz_1 - 1][1], matrices[matriz_1 - 1][0], matrices[matriz_2 - 1][1], matrices[matriz_2 - 1][0])
+            print(matriz_respuesta, "\n")
+            input("Presione Enter para continuar...")
+        else:
+            print("Las matrices 1 y 2 no se pueden multiplicar porque el número de columnas")
+            print("de la matriz 1 es diferente al número de filas de la matriz 2.")
+
+        return matrices, matriz_respuesta, True
 
     if opcion == 5:
-        print(transpuesta(matriz_2, dim_matriz_2), "\n")
+        while True:
+            try:
+                borrar_pantalla()
+                print("Hay disponibles %d matrices." %(len(matrices)))
+                num_matriz_1 = int(input("Seleccione matriz: "))
+            except ValueError:
+                continue
+            else:
+                break
+
+        matriz_respuesta = transpuesta(matrices[num_matriz_1 - 1][1], matrices[num_matriz_1 - 1][0])
+        print(matriz_respuesta, "\n")
+        input("Presione Enter para continuar...")
+
+        return matrices, matriz_respuesta, True
 
     if opcion == 6:
-        menor = matriz_1[0, 0]
-        mayor = matriz_1[0, 0]
+        while True:
+            try:
+                borrar_pantalla()
+                print("Hay disponibles %d matrices." %(len(matrices)))
+                num_matriz_1 = int(input("Seleccione matriz: "))
+            except ValueError:
+                continue
+            else:
+                break
+        
+        menor = matrices[num_matriz_1 - 1][1][0, 0]
+        mayor = matrices[num_matriz_1 - 1][1][0, 0]
 
-        menor, mayor = numero_menor_mayor(matriz_1, dim_matriz_1, menor, mayor)
+        menor, mayor = numero_menor_mayor(matrices[num_matriz_1 - 1][1], matrices[num_matriz_1 - 1][0], menor, mayor)
         print("El número menor es " + str(menor) + " y el número mayor es " + str(mayor) + "\n")
+        input("Presione Enter para continuar...")
+
+        return matrices, matriz_respuesta, True
 
     if opcion == 7:
-        menor = matriz_2[0, 0]
-        mayor = matriz_2[0, 0]
-
-        menor, mayor = numero_menor_mayor(matriz_2, dim_matriz_2, menor, mayor)
-        print("El número menor es " + str(menor) + " y el número mayor es " + str(mayor) + "\n")
-
-    # if opcion == 8:
-        # Graficar_matrices.main(n, matriz_1, matriz_2)
+        return matrices, matriz_respuesta, False
 
 def main():
-    operacion = 0
-    while operacion != 9:
-        # entrada_usuario()
-        dim_matriz_1 = dimensiones_matriz_1()
-        print(dim_matriz_1)
-        dim_matriz_2 = dimensiones_matriz_2()
-        print(dim_matriz_2)
-        matriz_1, matriz_2 = crear_matrices(dim_matriz_1, dim_matriz_2)
-        matriz_1 = llenar_matriz(dim_matriz_1, matriz_1)
-        matriz_2 = llenar_matriz(dim_matriz_2, matriz_2)
-        opcion = menu()
+    matrices = [[]]
+    matriz_respuesta = "No hay matriz respuesta."
 
+    while True:
         borrar_pantalla()
+        opcion = menu()
+        matrices, matriz_respuesta, continuar = opciones_menu(opcion, matrices, matriz_respuesta)
 
-        opciones_menu(opcion, matriz_1, dim_matriz_1, matriz_2, dim_matriz_2)
+        if continuar == False:
+            break
 
 main()
